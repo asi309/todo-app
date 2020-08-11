ede8564415a051bcb552c104e167bfde59365f78
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2020-08-12 00:10:40 +0530

    Added todo operations endpoint

---
+++ src/controllers/TodosController.js
@@ -0,0 +1,128 @@
+const Todos = require('../models/Todos');
+const User = require('../models/Users');
+
+module.exports = {
+    async createTodo (req, res) {
+        const { title, description, startDate, targetDate } = req.body;
+        const { user_id } = req.headers;
+        try {
+
+            const user = await User.findById(user_id);
+
+            if (!user) {
+                return res.status(400).json({
+                    message: 'User doesnot exist'
+                });
+            }
+    
+            if (!title) {
+                return res.status(200).json({
+                    message: 'Required field missing'
+                });
+            }
+    
+            const existing_todo = await Todos.findOne({ title });
+    
+            if (!existing_todo) {
+                const todo = await Todos.create({
+                    title,
+                    description,
+                    startDate,
+                    targetDate,
+                    user: user_id
+                });
+
+                await todo
+                        .populate('user', '-password')
+                        .execPopulate();
+    
+                return res.json(todo);
+            }
+    
+            return res.status(200).json({
+                message: 'Todo with title already exists'
+            });
+
+        } catch (error) {
+            throw Error(`Error while creating todo - ${error}`);
+        }
+    },
+
+    async deleteTodo (req, res) {
+        const { todoId } = req.params;
+        const { user_id } = req.headers;
+
+        try {
+            const user = await User.findById(user_id);
+            if (!user) {
+                return res.json({
+                    message: 'Cannot delete todo'
+                });
+            }
+
+            await Todos.findOneAndDelete({
+                _id: todoId,
+                user: user_id
+            });
+            
+            return res.status(204).send();
+        } catch (error) {
+            return res.status(400).json({
+                message: `Error in deleting todo - ${error}`
+            })
+        }
+    },
+
+    async getTodoById (req, res) {
+        const { todoId }= req.params;
+        const { user_id }= req.headers;
+
+        try {
+            const user = await User.findById(user_id);
+            if(!user_id) {
+                res.status(400).json({
+                    message: 'Cannot fetch todo'
+                });
+            }
+
+            const todo = await Todos.find({
+                _id: todoId,
+                user
+            });
+
+            if (todo) {
+                return res.json(todo);
+            }
+        } catch (error) {
+            return res.status(404).json({
+                message: `Todo not found - ${error}`
+            });
+        }
+
+    },
+
+    async getAllTodos (req, res) {
+        const { user_id } = req.headers;
+
+        try {
+            const user = await User.findById(user_id);
+
+            if (!user) {
+                return res.status(400).json({
+                    message: 'Cannot fetch todo'
+                });
+            }
+
+            const todos = await Todos.find({user});
+
+            if (todos) {
+                return res.json(todos);
+            }
+
+        } catch (error) {
+            res.status(400).json({
+                message: 'No todos yet'
+            });
+        }
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const LoginController = require('./controllers/LoginController');
+const TodosController = require('./controllers/TodosController');
 const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
@@ -13,6 +14,12 @@ routes.get('/', (req, res) => {
 
 //Login
 routes.post('/login', LoginController.store);
+
+//Todos
+routes.post('/todos/add', TodosController.createTodo);
+routes.get('/todos/:todoId', TodosController.getTodoById);
+routes.get('/todos/', TodosController.getAllTodos);
+routes.delete('/todos/delete/:todoId', TodosController.deleteTodo);
 
 //User Registration
 routes.post('/users/register', UserController.createUser);
